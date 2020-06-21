67178669b4a7d2eaa73f60e4245709641e35930e
王钦弘 <wangqinhong@bytedance.com>
2020-06-21 22:40:06 +0800

    feat: 🎸 完成【中缀表达式转后缀表达式】

--- CompilingPrinciple/1.0/index.ts
+++ CompilingPrinciple/1.0/index.ts
@@ -1,1 +1,21 @@
-console.log(1);
+import { getReversePolishExpression } from './solution';
+import * as readline from 'readline';
+import * as chalk from 'chalk';
+
+const sc = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log(chalk.blueBright('输入中缀四则运算（可以携带括号）表达式'));
+sc.on('line', input => {
+  console.log(`中缀表达式：${input}`);
+  console.log(`后缀表达式：${getReversePolishExpression(input)}`);
+
+  sc.close();
+});
+
+sc.on('close', () => {
+  console.log(chalk.blueBright('Process exit'));
+  process.exit(0);
+});
---
+++ CompilingPrinciple/1.0/solution.ts
@@ -0,0 +1,94 @@
+export const getReversePolishExpression = (value: string): string => {
+  // 输入中缀表达式，返回后缀表达式
+
+  // 模拟两个 stack
+  const numberQueue: Array<string> = []; // 数字队列
+  const operatorStack: Array<string> = []; // 运算符栈
+
+  // 维护符号栈和数字队列的 push 次数
+  // 因为加减乘除都是二元运算符，不计括号
+  // 那么数字队列的数字入队次数将会永远比符号栈的入栈次数多 1
+  let numberQueuePushCounter = 0;
+  let operatorStackPushCounter = 0;
+
+  value = value.trim(); // 忽略首尾空格
+
+  // 遍历 value
+  let numberBuffer: Array<string> = []; // 转存多位数字
+  value.split('').forEach((v: string, index: number) => {
+    if (v === ' ') {
+      // 忽略内部空格
+      return;
+    }
+    if (!isNaN(parseInt(v))) {
+      // 数字，存入缓冲区
+      numberBuffer.push(v);
+      return;
+    }
+
+    // 符号
+    // 如果缓冲区内存在数字，出队缓冲区内部所有数字，并重置缓冲区
+    if (numberBuffer.length) {
+      numberQueue.push(numberBuffer.join(''));
+      numberBuffer = [];
+      numberQueuePushCounter++; // 为一整个数字增加一次入队次数
+    }
+    if (!operatorStack.length) {
+      // 符号栈为空，压栈即可
+      operatorStack.push(v);
+      return;
+    }
+    // 符号栈不为空
+    if (v === '(' || v === '*' || v === '/') {
+      // 左括号、乘除号可以直接入栈
+      operatorStack.push(v);
+      if (v !== '(') {
+        operatorStackPushCounter++;
+      }
+      return;
+    }
+    const operatorStackTop = operatorStack[operatorStack.length - 1]; // 找到符号栈顶元素
+
+    if (v !== ')') {
+      // 加号或减号
+      if (operatorStackTop !== '*' && operatorStackTop !== '/') {
+        // 没有出现运算优先级问题，可以直接入栈
+        operatorStack.push(v);
+        return;
+      }
+      // 出现了运算优先级问题，要取出当前符号栈顶元素，并将其放入数字队列中
+      // 然后再将当前符号入符号栈
+      numberQueue.push(operatorStack.pop());
+      operatorStack.push(v);
+      operatorStackPushCounter++; // 只为新增的符号增加入栈次数
+      return;
+    }
+    // 右括号，需要一直出符号栈至数字队列内，直至遇到左括号
+    while (operatorStack[operatorStack.length - 1] !== '(') {
+      numberQueue.push(operatorStack.pop());
+      if (!operatorStack.length) {
+        // 异常处理
+        throw new Error(`错误的语法！没有一个可以匹配下标 ${index} 处右括号的左侧括号`);
+      }
+    }
+    // 最后还需要将当前这个 ( 出栈
+    operatorStack.pop();
+  });
+
+  // 取出最后存在于缓冲区的数字
+  // 最后一位一定是数字，
+  if (numberBuffer.length) {
+    numberQueue.push(numberBuffer.join(''));
+  }
+
+  if (numberQueuePushCounter - 1 !== operatorStackPushCounter) {
+    throw new Error('数字和符号个数不匹配！');
+  }
+
+  // 最后将所有符号栈内的元素，依次出栈至数字队列内
+  while (operatorStack.length) {
+    numberQueue.push(operatorStack.pop());
+  }
+
+  return numberQueue.join(' '); // 数字队列的出队结果，就是后缀表达式
+};
